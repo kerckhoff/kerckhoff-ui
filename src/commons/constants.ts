0419acf9b94b024616efa99c6e29a0d5f0d421bd
Zhang Hongyi <hongee@users.noreply.github.com>
2019-02-13 11:17:26 -0800

    Update constants.ts

--- src/commons/constants.ts
+++ src/commons/constants.ts
@@ -1,11 +1,11 @@
 export const KERCKHOFF_URL =
   process.env.NODE_ENV === 'production' ?
-    process.env.API_URL :
+    process.env.REACT_APP_API_URL :
     "https://kerckhoff-dev.herokuapp.com";
 
 export const SITE_URL =
   process.env.NODE_ENV === 'production' ?
-    process.env.SITE_URL :
+    process.env.REACT_APP_SITE_URL :
     "http://localhost:3000";
 
 export const OAUTH_URL = `${KERCKHOFF_URL}/api-oauth/google/auth`;
